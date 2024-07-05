d5e71395b5e23e2999c6b15eea9461cd7aa6384d
tyleramber <167348864+tyleramber@users.noreply.github.com>
2024-07-05 15:06:36 -0500

    Update Data_Analysis.sql

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -21,15 +21,10 @@ FROM dept_manager de
 JOIN employees e ON de.emp_no = e.emp_no
 JOIN departments d ON de.dept_no = d.dept_no;
 
-
-
-
-
 List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
-
 
 List each employee in the Sales department, including their employee number, last name, and first name.
 SELECT e.emp_no, e.last_name, e.first_name
@@ -38,16 +33,12 @@ JOIN dept_manager dm ON e.emp_no = dm.emp_no
 JOIN departments d ON dm.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
-
-
-
 List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 JOIN dept_manager dm ON e.emp_no = dm.emp_no
 JOIN departments d ON dm.dept_no = d.dept_no
 WHERE d.dept_name IN ('Sales', 'Development');
-
 
 List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, COUNT(*) AS name_count
